25fd24e4c266f9467d033495df3d5f224928908d
leomols97 <leomols@hotmail.com>
2022-10-26 12:37:31 +0200

    TD03 - Menu done

--- td05/__manifest__.py
+++ td05/__manifest__.py
@@ -29,6 +29,7 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/todo_menu.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
